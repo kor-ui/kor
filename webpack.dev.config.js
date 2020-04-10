9e78c98d54650ddd271bb0fee0f607e9c96eb352
Fernandes Ferreira <eduardo.fernandes_ferreira@siemens-healthineers.com>
2020-04-10 12:50:31 +0200

    [done] terser minification

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
 const dist = path.resolve(__dirname, './');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.ts",
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js$/,
+        terserOptions: {
+          // mangle: true, // Note `mangle.properties` is `false` by default.
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
   output: {
     filename: "kor.js",
     path: dist
@@ -25,7 +39,7 @@ module.exports = {
     contentBase: dist,
     compress: false,
     port: 8082,
-    host : '0.0.0.0'
+    host: '0.0.0.0'
   },
   plugins: [
     new HtmlWebpackPlugin({
