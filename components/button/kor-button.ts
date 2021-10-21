6194568a8c04bbee0c0b98a4fa6fd65b8b897919
Eduardo Ferreira <eduardo.fernandes_ferreira@siemens-healthineers.com>
2021-10-21 14:05:00 -0400

    [resolves #100] added icon + label button variation

--- components/button/kor-button.ts
+++ components/button/kor-button.ts
@@ -1,7 +1,7 @@
-import { LitElement, css, html } from 'lit';
-import { property } from 'lit/decorators';
-import { sharedStyles } from '../../shared-styles';
-import '../icon';
+import { LitElement, css, html } from "lit";
+import { property } from "lit/decorators";
+import { sharedStyles } from "../../shared-styles";
+import "../icon";
 
 /**
  * @prop {String} label -	Defines the text label.
@@ -11,102 +11,104 @@ import '../icon';
  */
 
 export class korButton extends LitElement {
-  @property({ type: String, reflect: true }) label = 'Label';
-  @property({ type: String, reflect: true }) icon: string | undefined;
-  @property({ type: String, reflect: true }) color:
-    | 'primary'
-    | 'secondary'
-    | 'tertiary' = 'primary';
-  @property({ type: Boolean, reflect: true }) disabled: boolean | undefined;
+	@property({ type: String, reflect: true }) label: string | undefined;
+	@property({ type: String, reflect: true }) icon: string | undefined;
+	@property({ type: String, reflect: true }) color:
+		| "primary"
+		| "secondary"
+		| "tertiary" = "primary";
+	@property({ type: Boolean, reflect: true }) disabled: boolean | undefined;
 
-  static get styles() {
-    return [
-      sharedStyles,
-      css`
-        :host {
-          font: var(--header-2);
-          color: var(--text-1);
-          display: flex;
-          height: max-content;
-          width: max-content;
-          border-radius: var(--border-radius);
-          cursor: pointer;
-          transition: var(--transition-1);
-          justify-content: center;
-          user-select: none;
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-        :host(:not([icon])) {
-          min-width: 56px;
-          max-width: 160px;
-          padding: 4px 12px;
-        }
-        :host([color='tertiary']:not([icon])) {
-          padding: 3px 11px;
-        }
-        :host([icon]) {
-          padding: 4px;
-        }
-        :host([color='tertiary'][icon]) {
-          padding: 3px;
-        }
-        /* idle */
-        :host([color='primary']) {
-          background-color: rgb(var(--accent-1));
-        }
-        :host([color='secondary']) {
-          background-color: rgba(var(--neutral-1), 0.1);
-        }
-        :host([color='tertiary']) {
-          border-width: 1px;
-          border-style: solid;
-          border-color: rgba(var(--neutral-1), 0.25);
-        }
-        /* disabled */
-        :host([disabled]) {
-          pointer-events: none;
-          opacity: 0.2;
-        }
-        /* text and icon colors */
-        kor-icon {
-          color: unset;
-        }
-        :host([color='primary']) {
-          color: rgba(255, 255, 255, 0.9);
-        }
-        /* hover inputs */
-        @media (hover: hover) {
-          :host([color='primary']:not(:active):hover) {
-            background-color: rgb(var(--accent-1b));
-          }
-          :host([color='secondary']:not(:active):hover) {
-            background-color: rgba(var(--neutral-1), 0.15);
-          }
-          :host([color='tertiary']:not(:active):hover) {
-            border-color: rgba(var(--neutral-1), 0.3);
-            background-color: rgba(var(--neutral-1), 0.05);
-          }
-        }
-      `,
-    ];
-  }
+	static get styles() {
+		return [
+			sharedStyles,
+			css`
+				:host {
+					font: var(--header-2);
+					color: var(--text-1);
+					display: flex;
+					gap: 4px;
+					height: max-content;
+					width: max-content;
+					border-radius: var(--border-radius);
+					cursor: pointer;
+					transition: var(--transition-1);
+					justify-content: center;
+					user-select: none;
+					white-space: nowrap;
+					overflow: hidden;
+					text-overflow: ellipsis;
+				}
+				:host([label]) {
+					min-width: 56px;
+					max-width: 160px;
+					padding: 4px 12px;
+				}
+				:host([color="tertiary"][label]) {
+					padding: 3px 11px;
+				}
+				:host(:not([label])) {
+					padding: 4px;
+				}
+				:host([color="tertiary"]:not([label])) {
+					padding: 3px;
+				}
+				/* idle */
+				:host([color="primary"]) {
+					background-color: rgb(var(--accent-1));
+				}
+				:host([color="secondary"]) {
+					background-color: rgba(var(--neutral-1), 0.1);
+				}
+				:host([color="tertiary"]) {
+					border-width: 1px;
+					border-style: solid;
+					border-color: rgba(var(--neutral-1), 0.25);
+				}
+				/* disabled */
+				:host([disabled]) {
+					pointer-events: none;
+					opacity: 0.2;
+				}
+				/* text and icon colors */
+				kor-icon {
+					color: unset;
+				}
+				:host([color="primary"]) {
+					color: rgba(255, 255, 255, 0.9);
+				}
+				/* hover inputs */
+				@media (hover: hover) {
+					:host([color="primary"]:not(:active):hover) {
+						background-color: rgb(var(--accent-1b));
+					}
+					:host([color="secondary"]:not(:active):hover) {
+						background-color: rgba(var(--neutral-1), 0.15);
+					}
+					:host([color="tertiary"]:not(:active):hover) {
+						border-color: rgba(var(--neutral-1), 0.3);
+						background-color: rgba(var(--neutral-1), 0.05);
+					}
+				}
+			`,
+		];
+	}
 
-  render() {
-    return html`
-      ${!this.icon
-        ? html` ${this.label} `
-        : html` <kor-icon icon="${this.icon}"></kor-icon> `}
-    `;
-  }
+	render() {
+		return html`
+			<slot name="icon">
+				${this.icon ? html` <kor-icon icon="${this.icon}"></kor-icon> ` : ""}
+			</slot>
+			<slot> ${this.label ? html` ${this.label} ` : ""}</slot>
+		`;
+	}
 
-  attributeChangedCallback(name: string, oldval: string, newval: string) {
-    super.attributeChangedCallback(name, oldval, newval);
-    this.dispatchEvent(new Event(`${name}-changed`));
-  }
+	attributeChangedCallback(name: string, oldval: string, newval: string) {
+		super.attributeChangedCallback(name, oldval, newval);
+		this.dispatchEvent(new Event(`${name}-changed`));
+	}
 }
 
-if (!window.customElements.get('kor-button')) {
-  window.customElements.define('kor-button', korButton);
+if (!window.customElements.get("kor-button")) {
+	window.customElements.define("kor-button", korButton);
 }
